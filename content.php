5daba8f341cd258c74e2516fb1fe71d4662ee5a6
Erick Hitter <ehitter@gmail.com>
2013-07-03 02:50:22 -0400

    Clean up how uploads are displayed, ensuring that files are linked to directly on singular views, and to singular views everywhere else.

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
 		<div class="entry-meta">
 			<?php wp_cloud_storage_posted_on(); ?>
@@ -19,13 +19,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wp_cloud_storage' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'wp_cloud_storage' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_attachment_link( get_the_ID(), true, null, ! is_singular() ); ?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
